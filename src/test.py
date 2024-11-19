91053269d8f736829c3cb1df03333637a044a84a
Takual <116489000+Takual@users.noreply.github.com>
2024-11-19 21:37:50 +0100

    Update Zahlen

--- src/test.py
+++ src/test.py
@@ -1,4 +1,7 @@
 import pandas as pd
+import csv
+import os
 
-df = pd.read_csv ("https://raw.githubusercontent.com/Takual/Energiepreise/refs/heads/main/data/raw/tbl_Vergleich_GV_GuenstjePostort_2Stichtage_Strom.csv?token=GHSAT0AAAAAACZ6C2USZ33U7TIKGL6QBMFYZZ446RQ")
-df.info(10)
+url = "https://raw.githubusercontent.com/Takual/Energiepreise/refs/heads/main/data/raw/tbl_Vergleich_GV_GuenstjePostort_2Stichtage_Strom.csv"
+data = pd.read_csv(url, delimiter=';')
+data.head(10)
