6b2e04acfd5a724342ebdf97164fdc755b508bda
Takual <116489000+Takual@users.noreply.github.com>
2024-11-19 19:26:54 +0100

    Update test.py

--- src/test.py
+++ src/test.py
@@ -1,4 +1,4 @@
 import pandas as pd
 
-df = pd.read_csv("https://vscode.dev/github/Takual/Energiepreise/blob/main/data/raw/tbl_Vergleich_GV_GuenstjePostort_2Stichtage_Strom.csv")
+df = pd.read_csv("data/raw/tbl_Vergleich_GV_GuenstjePostort_2Stichtage_Strom.csv")
 print(df.head(10))
